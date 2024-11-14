6eea5a3005f51cb7ef76a7140036111e92b0b8c5
helenajullia <helenajulliapastore@yahoo.com>
2024-11-14 18:12:04 +0200

    First commit - initialize project & setup the database

---
+++ src/main/java/com/rescueme/RescuemeApplication.java
@@ -0,0 +1,13 @@
+package com.rescueme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RescuemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RescuemeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rescueme/RescuemeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rescueme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescuemeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
